edfa92b0faac012ab74124bcdbe17cb3e8d71416
Aethersailor <406247832@qq.com>
2024-05-10 01:58:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,18 +22,20 @@ echo "赋予脚本文件权限..."
 chmod +x "$SCRIPT_PATH"
 
 # 修改 dnsmasq 配置文件取消忽略 hosts 文件的设置，删除包含 "option nohosts" 的行
-echo "修改 dnsmasq 配置文件，取消忽略 hosts 文件..."
+echo "修改 dnsmasq 配置文件，取消“忽略 hosts 文件”的设定..."
 sed -i '/option nohosts/d' "$DNSMASQ_CONF"
 
-# 检查是否已经存在相同的定时任务，如果不存在则添加
+echo "开始添加计划任务..."
+
+# 检查是否已经存在相同的计划任务，如果不存在则添加
 if grep -qF "$SCRIPT_PATH" /etc/crontabs/root; then
-    echo "已经存在相同的定时任务，跳过定时任务添加步骤。"
+    echo "已经存在相同的计划任务，跳过计划任务添加步骤。"
 else
-    echo "添加定时任务，默认更新时间为每天4:30..."
+    echo "添加计划任务，默认更新时间为每天4:30..."
     echo "$CRON_TIME $SCRIPT_PATH" >> /etc/crontabs/root
 fi
 
-# 提取定时任务的时间，并输出相应提示
+# 提取计划任务的时间，并输出相应提示
 CRON_HOUR=$(echo $CRON_TIME | cut -d' ' -f2)
 CRON_MIN=$(echo $CRON_TIME | cut -d' ' -f1)
 echo "AutoUpdateHosts 脚本安装完成！脚本将在每天的 $CRON_HOUR 点 $CRON_MIN 分运行。"
